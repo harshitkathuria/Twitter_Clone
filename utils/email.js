bd346d1663e531342fd9174c8909b2ccd78f411c
harshitkathuria <harshitkathuria14@gmail.com>
2021-06-11 14:32:59 +0530

    Configured nodemailer with gmail

--- utils/email.js
+++ utils/email.js
@@ -4,17 +4,17 @@ module.exports = class Email {
   constructor(user) {
     this.to = user.email;
     this.firstName = user.name.split(" ")[0];
-    this.from = `Administrator <${process.env.EMAIL_FROM}>`;
+    this.from = `${process.env.EMAIL_FROM}`;
   }
 
   newTransport() {
     return nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
+      service: 'gmail',
+      host: 'smtp.gmail.com',
       auth: {
         user: process.env.EMAIL_USERNAME,
-        pass: process.env.EMAIL_PASSWORD
-      }
+        pass: process.env.EMAIL_PASSWORD,
+      },
     });
   }
 
