4300be2d39e37e017755a0d434fd7faa643451ac
harshitkathuria <harshitkathuria14@gmail.com>
2021-06-12 10:38:15 +0530

    Fixed: User Pre Middleware

--- models/User.js
+++ models/User.js
@@ -59,7 +59,7 @@ userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 12);
-  this.passwordConfirm = undefined;
+  this.confirmPassword = undefined;
 
   next();
 });
