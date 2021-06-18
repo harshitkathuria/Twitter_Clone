ef37942fd8d786ad19b81c55225b88baba732719
harshitkathuria <harshitkathuria14@gmail.com>
2021-06-18 18:02:56 +0530

    Basic Cleanup and setting up tailwind css

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="w-full h-screen flex justify-center items-center">
+        <h1 className="font-bold text-5xl text-indigo-600">
+          Hello Tailwind CSS
+        </h1>
+      </div>
     </div>
   );
 }
