297ed899300147d5c6baeb5c55858e7cdb933947
harshitkathuria <harshitkathuria14@gmail.com>
2021-06-22 12:50:26 +0530

    Fixed: Combined with alert reducer

--- client/src/redux/reducers/index.js
+++ client/src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import alertReducer from "./alertReducer";
 
 const rootReducer = combineReducers({
-  auth: authReducer
+  auth: authReducer,
+  alert: alertReducer
 });
 
 export default rootReducer;
