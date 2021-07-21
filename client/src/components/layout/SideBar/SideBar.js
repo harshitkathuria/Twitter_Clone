13a3b92515f7302cb4d16c4bb670dfc882606580
harshitkathuria <harshitkathuria14@gmail.com>
2021-07-21 23:37:17 +0530

    Fixed: Tweet Button working on SideBar

--- client/src/components/layout/SideBar/SideBar.js
+++ client/src/components/layout/SideBar/SideBar.js
@@ -19,6 +19,11 @@ const SideBar = () => {
     dispatch(logout());
   };
 
+  const onTweetButton = () => {
+    if (window.location.pathname !== "/home") window.location.href = "/home";
+    document.getElementById("tweet-text").focus();
+  };
+
   return (
     <aside className="max-w-xs w-1/5 ml-32 py-1 h-screen max-h-screen border-r-2 border-gray-200 dark:border-gray-secondary fixed top-0 left-0 z-10 overflow-y-auto overflow-x-hidden">
       <div className="flex flex-col justify-between h-full">
@@ -28,7 +33,10 @@ const SideBar = () => {
           </div>
           <List />
           <div className="tweet-btn">
-            <button className="bg-blue text-white w-48 mt-3 hover:bg-blue bg-opacity-90 font-bold py-2 px-4 rounded-full">
+            <button
+              onClick={onTweetButton}
+              className="bg-blue text-white w-48 mt-3 hover:bg-blue bg-opacity-90 font-bold py-2 px-4 rounded-full focus:outline-none"
+            >
               Tweet
             </button>
           </div>
